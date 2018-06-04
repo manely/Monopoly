01b313b25a2e131e71f5a8b80925a53d5065f733
Mani Ghasemnia Hamedani <mani.hamedani@me.com>
2018-06-04 18:26:13 +0430

    Initial Commit

---
+++ Monopoly/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Monopoly
+//
+//  Created by Mani Hamedani on 6/4/18.
+//  Copyright © 2018 Manely. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
